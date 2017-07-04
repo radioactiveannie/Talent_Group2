f6620115bc1f4d51afa629ff19db6b06becb05a9
Tor Djärv <djarv@chalmers.se>
2017-07-05 00:10:34 +0200

    Created a basic NetBeans project, so far I have done nothing but made a dummy program

---
+++ project/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: djarv
+ *
+ * Created on den 5 juli 2017, 00:05
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"This is just a dummy"<<endl;
+    return 0;
+}
+
